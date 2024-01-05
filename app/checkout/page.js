521c85f849b13867c462a2f346cec5fd1de60c11
isksariful <cessinideveloper@gmail.com>
2024-01-05 15:28:21 +0530

    ne

--- app/checkout/page.js
+++
@@ -1,9 +0,0 @@
-import Checkoutmain from "@/components/Checkoutcomp/Checkoutmain"
-
-const CheckoutPage = () => {
-  return (
-    <Checkoutmain />
-  )
-}
-
-export default CheckoutPage
--- app/payment/page.js
+++
@@ -1,9 +0,0 @@
-import Paymentmain from "@/components/Checkoutcomp/Paymentmain"
-
-const payment = () => {
-  return (
-    <Paymentmain/>
-    )
-}
-
-export default payment
